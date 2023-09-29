9d78c4411198c12c5fca0bea0cc72b2d0fb6e9f5
Vinicius Pereira <niviciusdev@gmail.com>
2023-09-29 15:43:25 -0300

    fix: done function and managed position, identation and etc;  Changes to be committed: 	modified:   README.md 	modified:   candy.c

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ft_dojos
 Dojos 42
+
+//7 8 5 3 2 1 1
+//1 1 1 1 1 1 1
+//1 2
+//1 3 1
+//1 3 2 1
+//1 3 2 2 1
+//1 3 2 2 2 1
+//1 3 2 2 2 1 1 <- final result from left to right
+
+//1 3 2 2 2 1 1
+//1 3 2 2 2 1 1
+//1 3 2 3 2 1 1
+//1 3 4 3 2 1 1
+//1 5 4 3 2 1 1
+//1 5 4 3 2 1 1 <- Final result right to left
+
+// total candies -> 17
--- candy.c
+++ candy.c
@@ -6,98 +6,116 @@
 /*   By: mgonzaga <mgonzaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 18:43:48 by mgonzaga          #+#    #+#             */
-/*   Updated: 2023/09/29 02:34:16 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/09/29 15:43:17 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <stdio.h>
 
-void print_vector(int *kids, int *doces, int size)
+static int	candy(int kids[], int sizekids);
+static void	print_vector(int *kids, int *candies, int size);
+static void	manage_candies(int kids[], int sizekids, int candies[]);
+static void	equality(int kids[], int sizekids, int candies[]);
+
+int	main(void)
 {
-	for(int i = 0; i < size; i++)
-		printf("Nota: %d - Doces: %d \n", kids[i], doces[i]);
+	/* dojo example */ //total 17 candies
+	int a[] = {7, 8, 5, 3, 2, 1, 1};
+	printf("Total Candies: %d\n\n", candy(a, sizeof(a) / sizeof(a[0])));
+
+	/* leetcode example */ //total 5 candies
+	int b[] = {1, 0, 2};
+	printf("Total Candies: %d\n\n", candy(b, sizeof(b) / sizeof(b[0])));
+
+	/* leetcode example */ //total 4 candies
+	int c[] = {1, 2, 2};
+	printf("Total Candies: %d\n", candy(c, sizeof(c) / sizeof(c[0])));
+	return (0);
 }
 
-void	manage_candies(int kids[], int sizekids, int doces[])
-{
-	int	i = 0;
-	while(i < sizekids)
-	{
-		if(kids[i] > kids[i + 1] && doces[i] <= doces[i + 1])
-		{
-			doces[i] = doces [i + 1] + 1;
-		}
-		else if (kids[i] < kids[i + 1] && doces[i + 1] <= doces[i])
-		{
-			doces[i + 1] = doces[i] + 1;
-		}
-		i++;
-	}
-}
-
-void	equality(int kids[], int sizekids, int doces[])
-{
-	int	i = sizekids - 2;
-	while(i  >= 0)
-	{
-		if(kids[i] > kids[i + 1] && doces[i] <= doces[i + 1])
-		{
-			doces[i] = doces [i + 1] + 1;
-		}
-		else if (kids[i] < kids[i + 1] && doces[i + 1] <= doces[i])
-		{
-			doces[i + 1] = doces[i] + 1;
-		}
-		i--;
-	}
-}
-
-int	candy(int kids[], int sizekids)
+/**
+ * @brief Function that randle with ratings and candies
+ * @param int Kids[] Array of ratings of each kid
+ * @param int sizekids Lenght of the array
+ * @return Sum of candies at the end of distribution of candies
+ */
+static int	candy(int kids[], int sizekids)
 {
 	int	i;
-	int	doces[sizekids];
+	int	candies[sizekids];
 	int	candysum;
 
 	candysum = 0;
 	i = 0;
 	for (int j = 0; j < sizekids; j++)
-		doces[j] = 1;
-	manage_candies(kids, sizekids, doces);
-	equality(kids, sizekids, doces);
-	print_vector(kids, doces, sizekids);
+		candies[j] = 1;
+	manage_candies(kids, sizekids, candies);
+	equality(kids, sizekids, candies);
+	print_vector(kids, candies, sizekids);
 	for(int i = 0; i < sizekids; i++)
-		candysum += doces[i];
+		candysum += candies[i];
 	return (candysum);
 }
 
-int	main(void)
+/**
+ * @brief This function just print side by side final value of the ratings and candies
+ * @param int *kids Array of ratings of each kid
+ * @param int size Lenght of the array
+ * @param int *candies Array of candies of each kid
+ * @return Sum of candies at the end of distribution of candies
+ */
+static void print_vector(int *kids, int *candies, int size)
 {
-	int a[] = {7, 8, 5, 3, 2, 1, 1};
-	printf("Total Candies: %d\n", candy(a, sizeof(a) / sizeof(a[0])));
-
-	int b[] = {1, 0, 2};
-	printf("\nTotal Candies: %d\n", candy(b, sizeof(b) / sizeof(b[0])));
-
-	int c[] = {1, 2, 2};
-	printf("\nTotal Candies: %d\n", candy(c, sizeof(c) / sizeof(c[0])));
-	return (0);
+	for(int i = 0; i < size; i++)
+		printf("Nota: %d - Doces: %d \n", kids[i], candies[i]);
 }
 
+/**
+ * @brief This function divide candies proportionally
+ * among the children based on your grades (from right to left)
+ * @param int *kids Array of grade of each kid
+ * @param int sizekids Lenght of the array of kids
+ * @param int *candies Array of candies of each kid
+ * @return Void
+ */
+static void	manage_candies(int kids[], int sizekids, int candies[])
+{
+	int	i = 0;
+	while(i < sizekids - 2)
+	{
+		if(kids[i] > kids[i + 1] && candies[i] <= candies[i + 1])
+		{
+			candies[i] = candies [i + 1] + 1;
+		}
+		else if (kids[i] < kids[i + 1] && candies[i + 1] <= candies[i])
+		{
+			candies[i + 1] = candies[i] + 1;
+		}
+		i++;
+	}
+}
 
-//7 8 5 3 2 1 1
-//1 1 1 1 1 1 1
-//1 2
-//1 3 1
-//1 3 2 1
-//1 3 2 2 1
-//1 3 2 2 2 1
-//1 3 2 2 2 1 1 <- final result from left to right
-
-//1 3 2 2 2 1 1
-//1 3 2 2 2 1 1
-//1 3 2 3 2 1 1
-//1 3 4 3 2 1 1
-//1 5 4 3 2 1 1
-//1 5 4 3 2 1 1 <- Final result right to left
-
+/**
+ * @brief This function divide candies proportionally
+ * among the children based on your grades (from left to right)
+ * @param int *kids Array of grade of each kid
+ * @param int sizekids Lenght of the array of kids
+ * @param int *candies Array of candies of each kid
+ * @return Void
+ */
+static void	equality(int kids[], int sizekids, int candies[])
+{
+	int	i = sizekids - 2;
+	while(i  >= 0)
+	{
+		if(kids[i] > kids[i + 1] && candies[i] <= candies[i + 1])
+		{
+			candies[i] = candies [i + 1] + 1;
+		}
+		else if (kids[i] < kids[i + 1] && candies[i + 1] <= candies[i])
+		{
+			candies[i + 1] = candies[i] + 1;
+		}
+		i--;
+	}
+}
